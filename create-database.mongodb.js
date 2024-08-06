0c6767feef2c744b97ed0d7d1ba079998b48514b
sr-paixao <henriquemfpaixao@gmail.com>
2024-08-06 20:34:29 -0300

    Crate database

---
+++ create-database.mongodb.js
@@ -0,0 +1,11 @@
+// Define o nome do banco de dados
+const database = 'BD3-HENRIQUE-AULA'
+
+// define o nome da collection
+const collection = 'LIVRARIA'
+
+// acessa o banco de dados libri-database
+use(database);
+
+//create colection
+db.createCollection(collection);
